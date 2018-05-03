dcccc3438e1ba1e419e6ef62770377f4ead49042
Kieran Thompson <l00118651@student.lyit.ie>
2018-05-03 13:03:17 +0100

    updatephp updates all fields of row

--- update.php
+++ update.php
@@ -3,7 +3,7 @@
 
     mysqli_select_db($conn, 'worldcup2018');
 
-    $update_sql = "UPDATE matches SET Team1Win='$_POST[team1win]' WHERE MatchID='$_POST[id]'"; 
+    $update_sql = "UPDATE matches SET Team1Win='$_POST[team1win]', Draw='$_POST[draw]', Team2Win='$_POST[team2win]' WHERE MatchID='$_POST[id]'"; 
 
     if(mysqli_query($conn, $update_sql)) 
             header("refresh:1; url=MatchOddsUpdate.php");
