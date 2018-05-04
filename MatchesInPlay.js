882120b5fc07ab7be41a64ff66b2ed38e9643d39
Kieran Thompson <l00118651@student.lyit.ie>
2018-05-04 11:52:18 +0100

    minor adjustments to matches in play and updatephp

--- MatchesInPlay.js
+++ MatchesInPlay.js
@@ -6,8 +6,10 @@ window.onload = () => {
         $.getJSON('MatchData.php', data => {
         templatearray = data;
         $.each(templatearray, (index, i) => {
-            $.extend(i, {team1abr: i.Team1.substring(0,3).toLowerCase()});
-            $.extend(i, {team2abr: i.Team2.substring(0,3).toLowerCase()});
+            $.extend(i, {team1img: i.Team1.substring(0,3).toLowerCase()});
+            $.extend(i, {team2img: i.Team2.substring(0,3).toLowerCase()});
+            $.extend(i, {team1abr: i.team1img.charAt(0).toUpperCase() + i.team1img.substring(1,3)});
+            $.extend(i, {team2abr: i.team2img.charAt(0).toUpperCase() + i.team2img.substring(1,3)});
         });
     console.log('5 second update');
     let template = $('#template').html();
